5e66cda9960970a27b1a29e5d5d1a7f2ee29f576
Esha Singh <singh.esha99999@gmail.com>
2024-12-02 16:36:08 +0100

    created favourite model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_02_152255) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_02_153314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_02_152255) do
     t.datetime "updated_at", null: false
     t.index ["property_id"], name: "index_bookings_on_property_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "property_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_favourites_on_property_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
   create_table "properties", force: :cascade do |t|
@@ -58,6 +67,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_02_152255) do
 
   add_foreign_key "bookings", "properties"
   add_foreign_key "bookings", "users"
+  add_foreign_key "favourites", "properties"
+  add_foreign_key "favourites", "users"
   add_foreign_key "reviews", "properties"
   add_foreign_key "reviews", "users"
 end
