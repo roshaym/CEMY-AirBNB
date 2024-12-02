7fb618a852120ccb627518a45dff9c05cdb32b3f
MATT ROSHAY <ROSHAYM@GMAIL.COM>
2024-12-02 15:34:32 +0100

    Created property model & properties controller

---
+++ db/migrate/20241202142709_create_properties.rb
@@ -0,0 +1,13 @@
+class CreateProperties < ActiveRecord::Migration[7.1]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.integer :rating
+      t.string :location
+      t.string :price_per_night
+      t.string :availability
+      
+      t.timestamps
+    end
+  end
+end
