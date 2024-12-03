7ff16e5b508bbf364a5202eb2fb63c4048248c3f
YahiaJabeur <yahia.jabeur@gmail.com>
2024-12-03 11:41:51 +0100

    devise configuration

--- db/migrate/20241202153314_create_favourites.rb
+++ db/migrate/20241202153314_create_favourites.rb
@@ -1,7 +1,7 @@
 class CreateFavourites < ActiveRecord::Migration[7.1]
   def change
     create_table :favourites do |t|
-      t.references :user, null: false, foreign_key: true
+      # t.references :user, null: false, foreign_key: true
       t.references :property, null: false, foreign_key: true
 
       t.timestamps
---
+++ db/migrate/20241203102646_remove_user_from_favourites.rb
@@ -0,0 +1,4 @@
+class RemoveUserFromFavourites < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
