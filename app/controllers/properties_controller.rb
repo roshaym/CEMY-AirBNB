3ad2a6b132feb9c36aa6af61dec4ba9f3b36bbe5
MATT ROSHAY <ROSHAYM@GMAIL.COM>
2024-12-03 16:33:40 +0100

    Added search functionality, navbar home button functionality and navbar logo to home functionality. Also updated seeds for rating to be 1 to 5 instead of 0 to 5.

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,22 @@
 class PropertiesController < ApplicationController
+  def index
+    @properties = Property.all
+    if params[:query].present?
+      @properties = @properties.where("location ILIKE ?", "%#{params[:query]}%")
+    end
+  end
+
+  # def search
+  #   if params[:query]
+  #     @property = @properties.select { |property| property.location.start_with?(params[:query]) }
+  #   end
+
+  # end
+
+  private
+
+  def properties_params
+    params.require(:property).permit(:query)
+  end
+
 end
