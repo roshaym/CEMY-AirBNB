a6a98df4b39ea3c602e18112c21e2c2d70b4d99a
Esha Singh <singh.esha99999@gmail.com>
2024-12-04 16:10:59 +0100

    added pictures to cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_132106) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_143730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_132106) do
     t.index ["property_id"], name: "index_favourites_on_property_id"
   end
 
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.string "location"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
   create_table "properties", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
@@ -43,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_132106) do
     t.bigint "user_id"
     t.float "latitude"
     t.float "longitude"
+    t.string "image_url"
     t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
@@ -71,5 +83,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_132106) do
 
   add_foreign_key "bookings", "properties"
   add_foreign_key "favourites", "properties"
+  add_foreign_key "listings", "users"
   add_foreign_key "reviews", "properties"
 end
