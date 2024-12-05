d0689325e16e4376707e2e03347c529e05c76c33
MATT ROSHAY <ROSHAYM@GMAIL.COM>
2024-12-05 16:20:29 +0100

    fixed property creation & deletion for current user

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,7 @@
 class PropertiesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   before_action :set_property, only: %i[show edit update destroy]
-  
+
   def index
     @properties = Property.all
 
@@ -45,10 +46,20 @@ class PropertiesController < ApplicationController
 
   # POST > create listings
   def create
+    unless current_user
+      redirect_to login_path, alert: 'You must be logged in to create a property.'
+      return
+    end
+
     @property = current_user.properties.build(property_params)
+    @property.rating ||= 0
     if @property.save
       redirect_to @property, notice: 'Property was successfully created.'
     else
+      # Rails.logger.debug @property.errors.full_messages.inspect
+      # Rails.logger.debug "Current User: #{current_user.inspect}"
+      # Rails.logger.debug "@property.user_id: #{@property.user_id.inspect}"
+      # puts @property.errors.full_messages
       render :new
     end
   end
@@ -68,8 +79,11 @@ class PropertiesController < ApplicationController
 
   # DELETE listing
   def destroy
+    @property = current_user.properties.find(params[:id]) # Ensures only the owner can delete
     @property.destroy
-    redirect_to property_url, notice: 'Property was successfully destroyed.'
+    redirect_to properties_path, notice: 'Property was successfully deleted.'
+  rescue ActiveRecord::RecordNotFound
+    redirect_to properties_path, alert: 'You are not authorized to delete this property.'
   end
 
 
@@ -80,7 +94,7 @@ class PropertiesController < ApplicationController
   end
 
   def property_params
-    params.require(:property).permit(:name, :description, :price_per_night, :address)
+    params.require(:property).permit(:name, :description, :price_per_night, :address, :user_id, :image_url)
   end
 
 end
