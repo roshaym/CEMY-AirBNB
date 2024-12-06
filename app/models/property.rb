d9f9f1f740904fda646f81594eec08ad840256f8
YahiaJabeur <yahia.jabeur@gmail.com>
2024-12-06 10:49:55 +0100

    upload property image

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,8 +1,28 @@
 class Property < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :image
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  validates :name, :description, :price_per_night, :rating, :image_url, :address, presence: true
+  validates :name, :description, :price_per_night, :rating, :address, presence: true
+  
+  # Replace the previous validation with these
+  validate :acceptable_image
+
+  private
+
+  def acceptable_image
+    return unless image.attached?
+
+    unless image.blob.byte_size <= 5.megabytes
+      errors.add(:image, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png", "image/jpg"]
+    unless acceptable_types.include?(image.blob.content_type)
+      errors.add(:image, "must be a JPEG, PNG, or JPG")
+    end
+  end
 end
